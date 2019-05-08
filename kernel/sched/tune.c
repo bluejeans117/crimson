7eb621f31679579c879d9eb1376b1a400c061c20
Yaroslav Furman <yaro330@gmail.com>
2019-05-08 15:09:37 -0400

    sched/tune: Introduce SchedTune Assist[v3]
    This implements a mechanism by which default SchedTune parameters
    can be configured in-kernel, circumventing userspace, and
    mitigating reliance on ramdisk modification in the context of
    custom kernels.
    
    [2.5V]: This version adds proper protection
    from userspace (mainly init) trying to write lame
    boost values and gives full control to developer
    and user (sh is not blocked).
    
    [V3.0]: Use a struct to store all the values.
    
    [0ctobot: Update for msm-4.9 and improve coding style]
    [yaro: Update for 4.14]
    Co-authored-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -696,6 +696,37 @@ schedtune_boostgroup_init(struct schedtune *st)
 	return 0;
 }
 
+#ifdef CONFIG_STUNE_ASSIST
+static void write_default_values(struct cgroup_subsys_state *css)
+{
+	u8 i;
+	struct groups_data {
+		char *name;
+		int boost;
+		bool prefer_idle;
+		bool colocate;
+		bool no_override;
+	};
+	struct groups_data groups[3] = {
+		{ "top-app",	5, 1, 0, 1 },
+		{ "foreground", 1, 1, 0, 1 },
+		{ "background", 0, 0, 1, 0 }};
+
+	for (i = 0; i < ARRAY_SIZE(groups); i++) {
+		if (!strcmp(css->cgroup->kn->name, groups[i].name)) {
+			pr_info("%s: %i - %i - %i - %i\n", groups[i].name,
+					groups[i].boost, groups[i].prefer_idle,
+					groups[i].colocate,
+					groups[i].no_override);
+			boost_write(css, NULL, groups[i].boost);
+			prefer_idle_write(css, NULL, groups[i].prefer_idle);
+			sched_colocate_write(css, NULL, groups[i].colocate);
+			sched_boost_override_write(css, NULL, groups[i].no_override);
+		}
+	}
+}
+#endif
+
 static struct cgroup_subsys_state *
 schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 {
